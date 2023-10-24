7fcdcac21d64f883e64f6bde3b6e635e6fff3f1b
5252osoi <napchiwang21@gmail.com>
2023-10-24 11:32:19 +0900

    2023241132

---
+++ reactwork/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      // javascript주석
+    <div className="App">
+      {/* jsx주석 */}
+      <header className="App-header">
+        <div className="title">React</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
